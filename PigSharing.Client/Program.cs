ed4c9dd3390781feba07f0cbb3ba754f3de79812
unknown <bouketin27@gmail.com>
2024-07-24 18:47:18 +0200

    Modif de la config du client

--- PigSharing.Client/Program.cs
+++ PigSharing.Client/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Hosting;
 using PigSharing.Client;
+
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,3 +11,8 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+        
+     
+
+
+
