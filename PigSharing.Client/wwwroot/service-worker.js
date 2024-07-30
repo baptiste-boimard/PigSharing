78adb3c6b79a0e0a9607f795c0407ff4b749daa0
unknown <bouketin27@gmail.com>
2024-07-31 01:43:10 +0200

    Modif des reglage pour la pwa

--- PigSharing.Client/wwwroot/service-worker.js
+++ PigSharing.Client/wwwroot/service-worker.js
@@ -1,4 +1,24 @@
-// In development, always fetch from the network and do not enable offline support.
-// This is because caching would make development more difficult (changes would not
-// be reflected on the first load after each change).
-self.addEventListener('fetch', () => { });
+self.addEventListener('install', event => {
+    console.log('Service Worker installing.');
+    // You can add pre-caching logic here
+});
+
+self.addEventListener('activate', event => {
+    console.log('Service Worker activating.');
+    // You can add cleanup logic here
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request).then(function(networkResponse) {
+                return caches.open('my-cache').then(function(cache) {
+                    cache.put(event.request, networkResponse.clone());
+                    return networkResponse;
+                });
+            });
+        }).catch(function() {
+            return new Response('Network request failed and no cache available');
+        })
+    );
+});
